5b338f33ab55aedfce40e0942ff392a96a470bc2
Bartosz Ptaszynski <foobarto@gmail.com>
2019-05-18 16:52:29 +0100

    correction to NOSTEAM

--- update.sh
+++ update.sh
@@ -29,9 +29,9 @@ sed -i -e s/RCONPassword=[[:space:]]/RCONPassword=${RCON_PASSWORD}/ $INIFILE
 
 echo -e "Launching server..."
 if [[ -z "${NOSTEAM}" ]]; then
-  /home/steam/projectzomboid/start-server.sh -servername ${SERVERNAME} -nosteam -steamport1 ${STEAMPORT1} -steamport2 ${STEAMPORT2} -adminpassword ${ADMINPASSWORD} &
-else
   /home/steam/projectzomboid/start-server.sh -servername ${SERVERNAME} -steamport1 ${STEAMPORT1} -steamport2 ${STEAMPORT2} -adminpassword ${ADMINPASSWORD} &
+else
+  /home/steam/projectzomboid/start-server.sh -servername ${SERVERNAME} -nosteam -steamport1 ${STEAMPORT1} -steamport2 ${STEAMPORT2} -adminpassword ${ADMINPASSWORD} &
 fi
 
 
